44addd46aaa8ae0e91cdfd72ee81343ec13f290c
马军伟 <majunwei@weidian.com>
2017-10-11 22:49:51 +0800

    高可用注册中心、服务提供者

--- spring-cloud-sample-tutorial-producer/src/main/java/com/majunwei/spring/cloud/sample/tutorial/producer/UserController.java
+++ spring-cloud-sample-tutorial-producer/src/main/java/com/majunwei/spring/cloud/sample/tutorial/producer/UserController.java
@@ -2,13 +2,16 @@ package com.majunwei.spring.cloud.sample.tutorial.producer;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by majunwei on 2017/10/10.
  */
-@Controller("user")
+@Controller
+@RequestMapping("user")
 public class UserController {
     @RequestMapping("add")
+    @ResponseBody
     public String addUser(String userName, String age) {
         return "Success";
     }
